99ca74d489f5914f1e13b50b478ea97117727e10
EDWARD <60771749+KiritoEdward@users.noreply.github.com>
2021-03-26 14:05:24 +0700

    Tugas 8 Javascript
    Tugas ini Saya kumpulkan untuk NIOMIC

---
+++ tugas8.js
@@ -0,0 +1,13 @@
+dataDiri = () => {
+  //nama mahasiswa adalah Ferdian
+  let mahasiswa = {
+    nama: "Ferdian",
+    jurusan: "Teknik Informatika",
+    angkatan: 2018,
+    kampus: "BINUS UNIVERISTY"
+  };
+  mahasiswa.nama = "Edward"; //setelah dirubah nama mahasiswa menjadi Edward
+  console.log(mahasiswa);
+  console.log(mahasiswa.nama);
+};
+dataDiri();
